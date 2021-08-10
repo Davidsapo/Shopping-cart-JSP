f6b14e5cc0b6100df831ea5ca4185eaea426c1be
David Sapoznik <davidsza1290@gmail.com>
2021-08-10 13:20:31 +0300

    'Update cart item' endpoint

--- src/main/java/com/shopping/cart/controller/CartController.java
+++ src/main/java/com/shopping/cart/controller/CartController.java
@@ -2,6 +2,7 @@ package com.shopping.cart.controller;
 
 import com.shopping.cart.dto.CartDTO;
 import com.shopping.cart.request.AddToCartRequest;
+import com.shopping.cart.request.UpdateCartItemRequest;
 import com.shopping.cart.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -30,34 +31,9 @@ public class CartController {
         return ResponseEntity.ok(cartService.addProduct(request.getPersonID(), request.getProductID(), request.getQuantity()));
     }
 
-    /*@PostMapping("add")
-    public ResponseEntity<Cart> add(long personId) {
-        return ResponseEntity.ok(cartService.addCart(personId));
+    @PutMapping("/update")
+    public ResponseEntity<CartDTO> updateCartItem(@RequestBody @Valid UpdateCartItemRequest request) {
+        return ResponseEntity.ok(cartService.updateCartItem(request.getPersonId(), request.getCartItemId(), request.getQuantity()));
     }
 
-    @GetMapping("list")
-    public ResponseEntity<List<Cart>> list(Long personId) {
-        return ResponseEntity.ok(cartService.getAllCarts(personId));
-    }
-
-    @PutMapping("/{cartId}/add/product")
-    public ResponseEntity<Cart> addProductToCart(@PathVariable Long cartId, Long productId, Integer quantity) {
-        if (productId == null || quantity == null) {
-            throw new CartException("Required parameters: productId, quantity");
-        }
-        return ResponseEntity.ok(cartService.addProductToCart(cartId, productId, quantity));
-    }
-
-    @PutMapping("/{cartId}/delete/product")
-    public ResponseEntity<Cart> deleteProductFromCart(@PathVariable Long cartId, Long productId) {
-        if (productId == null) {
-            throw new CartException("Required parameters: productId");
-        }
-        return ResponseEntity.ok(cartService.deleteProductFromCart(cartId, productId));
-    }
-
-    @DeleteMapping("delete/{cartId}")
-    public ResponseEntity<String> delete(@PathVariable long cartId) {
-        return ResponseEntity.ok(cartService.deleteById(cartId));
-    }*/
 }
